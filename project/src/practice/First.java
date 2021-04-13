8607a5bc434f47a3aeaf8130350cbc781937098a
ayushchhajed <ayush.chhajed.ac@gmail.com>
2021-04-14 01:10:51 +0530

    commit

---
+++ project/src/practice/First.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Ayush");
+		
+		int one = 'A'/'A';
+		String s1 = "..........";
+		
+		for (int i=one ; i<=(s1.length() * s1.length()); i++)
+		{
+			
+		System.out.println(+i);
+	}
+	}
+
+}
